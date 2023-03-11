a3a575b9f131d3089f61af151b0373af4d7498ed
Klasique <feboapong@gmail.com>
2023-03-11 21:11:00 +0100

    yes

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 path('', views.home, name='home'),
@@ -8,3 +10,7 @@ path('contact/', views.contact, name='contact'),
 path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
    path('search/', views.search, name='search'),
 ]
+
+#if settings.DEBUG:
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
