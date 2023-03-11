10f262c38c4b2ee19ecc536c2bd8cbd869c1450c
Klasique <feboapong@gmail.com>
2023-03-11 23:54:44 +0100

    yes

--- blog/urls.py
+++ blog/urls.py
@@ -4,10 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-path('', views.home, name='home'),
-path('detail/<int:pk>/<slug:slug>/<int:year>/<int:month>/<int:day>/', views.detail, name='detail'),
-path('contact/', views.contact, name='contact'),
-path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+   path('privacy/', views.privacy, name='privacy'),
+   path('terms/', views.terms, name='terms'),
+   path('', views.home, name='home'),
+   path('detail/<int:pk>/<slug:slug>/<int:year>/<int:month>/<int:day>/', views.detail, name='detail'),
+   path('contact/', views.contact, name='contact'), 
+   path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
    path('search/', views.search, name='search'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -15,6 +15,12 @@ def home(request):
     tags = Tag.objects.all()
     return render(request, 'home/home.html', {'tags_all':tags})
 
+def privacy(request):
+    return render(request, 'privacy.html')
+
+def terms(request):
+    return render(request, 'terms.html')
+
 def contact(request):
     pass
 
